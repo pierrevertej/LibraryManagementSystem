36eb45ae5c337cc6fa4e9857cd9e65af3469bf4e
pierrevertej <pierrevertej@gmail.com>
2026-05-14 21:35:59 -0400

    teacher

--- src/main/java/org/pierrevertej/Teacher.java
+++ src/main/java/org/pierrevertej/Teacher.java
@@ -1,4 +1,42 @@
 package org.pierrevertej;
 
-public class Teacher {
+import util.Constants;
+
+import java.util.List;
+
+public class Teacher extends User {
+    public Teacher(String name) {
+        super(name);
+    }
+
+    public Teacher(String id, String name, List<Item> borrowedItems) {
+        super(id, name, borrowedItems);
+    }
+
+    @Override
+    public boolean borrowItem(Item item) {
+        if (canBorrowItem(item)) {
+            borrowedItems.add(item);
+            item.setStatus(Item.Status.BORROWED);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean returnItem(Item item) {
+        if (borrowedItems.contains(item)) {
+            borrowedItems.remove(item);
+            item.setStatus(Item.Status.INSTORE);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean canBorrowItem(Item item) {
+        return !borrowedItems.contains(item) && item.getStatus() == Item.Status.INSTORE && item instanceof Book && borrowedItems.size() < Constants.MAX_ITEMS_TEACHER;
+    }
 }
