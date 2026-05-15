c64d28848a4d7ec6bce82d9d20b167e5cb7b36d8
pierrevertej <pierrevertej@gmail.com>
2026-05-14 21:18:46 -0400

    user

--- src/main/java/org/pierrevertej/User.java
+++ src/main/java/org/pierrevertej/User.java
@@ -1,4 +1,13 @@
 package org.pierrevertej;
 
-public class User {
+import java.util.List;
+
+public abstract class User {
+    protected String id;
+    protected String name;
+    protected List<Item> borrowedItems;
+
+    public boolean borrowItem;
+    public boolean returnItem;
+    public boolean canBorrowItem;
 }
