e89d12b5586bdfb2ab19ef165f621ca7f429d777
pierrevertej <pierrevertej@gmail.com>
2026-05-14 21:15:57 -0400

    magazine

--- src/main/java/org/pierrevertej/Magazine.java
+++ src/main/java/org/pierrevertej/Magazine.java
@@ -1,4 +1,13 @@
 package org.pierrevertej;
 
-public class Magazine {
+public class Magazine extends Item {
+    private String title;
+    private String issueNumber;
+    private String publisher;
+
+    public Magazine(String title, String issueNumber, String publisher) {
+        this.title = title;
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
