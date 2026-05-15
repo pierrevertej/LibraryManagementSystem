d14a45ecaa3944ad2c7c99517f914255a3e6fc8d
pierrevertej <pierrevertej@gmail.com>
2026-05-14 21:05:59 -0400

    item

--- src/main/java/org/pierrevertej/Item.java
+++ src/main/java/org/pierrevertej/Item.java
@@ -1,5 +1,24 @@
 package org.pierrevertej;
 
-public class Item {
-    
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public abstract class Item {
+    protected String id;
+    @Setter
+    protected Status status;
+
+    @Setter private static int nextId = 1;
+
+    public Item() {
+        this.id = String.format("%04d", nextId++);
+        this.status = Status.INSTORE;
+    }
+
+    public enum Status {
+        INSTORE, BORROWED, LOST
+    }
 }
