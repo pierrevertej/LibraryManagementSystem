cca4e39788cd88005f89aa8af4056bb51ec06d16
pierrevertej <pierrevertej@gmail.com>
2026-05-14 21:12:42 -0400

    book

--- src/main/java/org/pierrevertej/Book.java
+++ src/main/java/org/pierrevertej/Book.java
@@ -1,4 +1,22 @@
 package org.pierrevertej;
 
-public class Book {
+public class Book extends Item {
+    private String isbn;
+    private String title;
+    private String author;
+    private String genre;
+
+    public Book(String id, Status status, String isbn, String title, String author, String genre) {
+        if (!isValidISBN(isbn)) {
+        }
+        super(id, status);
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+    }
+
+    public static boolean isValidISBN(String isbn) {
+        return isbn.matches("\\d{13}"); // Must be 13 digits
+    }
 }
